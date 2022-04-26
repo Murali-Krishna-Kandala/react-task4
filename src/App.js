b2b6bb65eca61f423a5856ab12cfede009c4d00c
Murali Krishna K <77782465+murali-krishna-naidu@users.noreply.github.com>
2022-04-26 08:19:46 +0530

    f-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Heading from "./components/Heading";
+import "./App.css";
+import Aboutus from "./components/Aboutus";
+import Best from "./components/Best";
+import AboutComp from "./components/AboutCompany";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Main />
+      <Heading />
+      <Aboutus />
+      <Best />
+      <AboutComp />
     </div>
   );
 }
